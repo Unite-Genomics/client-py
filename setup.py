ed7710d490f0c19c2daa6df069a8501394a4e03a
Damjan Ravnjak <damjan@unitegenomics.com>
2025-05-23 01:09:05 +0200

    EN-4906: Minor update

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     author="SMART Platforms Team",
     author_email='support@smarthealthit.org',
     packages=find_packages(exclude=['test*', '*_tests.py']),
-    package_dir={'fhirclientdstu2': 'fhirclient'},
+    package_dir={'': 'fhirclient'},
     install_requires=['requests', 'isodate'],
     classifiers=[
         'Development Status :: 4 - Beta',
